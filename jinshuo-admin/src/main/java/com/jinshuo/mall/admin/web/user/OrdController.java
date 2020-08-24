d2a0cc0f2a2cee21a038cc48c06267dee577ec37
dyhimos <himos@163.com>
2020-08-24 19:10:13 +0800

    订单

---
+++ jinshuo-admin/src/main/java/com/jinshuo/mall/admin/web/user/OrdController.java
@@ -0,0 +1,16 @@
+package com.jinshuo.mall.admin.web.user;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2020/8/16.
+ */
+@Slf4j
+@RestController
+@Api(description = "订单接口")
+@RequestMapping("/v1/h5/user/ordmain")
+public class OrdController {
+}
